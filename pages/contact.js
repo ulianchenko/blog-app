822ead4b84c6feab25c71628b73958a33d5827e8
ulianchenko <ulianchenko@gmail.com>
2025-02-06 02:04:29 +0200

    the contat-from component was added

--- pages/contact.js
+++ pages/contact.js
@@ -1,5 +1,17 @@
-function ContactPage() {
+// import Head from "next/head";
 
+import ContactForm from "../components/contact/contact-form";
+
+function ContactPage() {
+  return (
+    <>
+      {/* <Head>
+        <title>Contact Me</title>
+        <meta name="description" content="Send me your messages!" />
+      </Head> */}
+      <ContactForm />
+    </>
+  );
 }
 
 export default ContactPage;
